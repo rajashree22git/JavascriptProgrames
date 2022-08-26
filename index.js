42012dbb4364293b64d2db9c688fbc2910462277
rajashree22git <rai.rajashree@gmail.com>
2022-08-26 17:41:06 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-console.log('raji');
+function FizzBuzz(n)
+{
+
+  for(let i=1;i<=n;i++)
+  {
+    if(i%5==0 && i%3==0)
+    {
+console.log("fizzBuzz");
+    }
+   else if(i%5==0)
+    {
+console.log("Bizz");
+    }
+
+   else if(i%3==0 )
+    {
+console.log("Fiz");
+    }
+    else{
+      console.log(i);
+    }
+
+  }
+
+}
 
 
+FizzBuzz(15);
